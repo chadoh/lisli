692d314ce989256977462899a29603f8be3e44fb
Chad Ostrowski <chad.ostrowski@gmail.com>
2010-04-26 08:54:05 -0400

    Correctly displaying one blog post from Blogger on the :thoughts page.
    Next: get all of them loaded in, and put this on Heroku.

--- controls.rb
+++ controls.rb
@@ -6,6 +6,16 @@ require 'pony'
 require 'crack'
 require 'open-uri'
 
+#Extend Hash class so that keys can be accessed as attributes, with dot notation.
+#Courtesy of http://www.goodercode.com/wp/convert-your-hash-keys-to-object-properties-in-ruby/
+class ::Hash
+  def method_missing(name)
+    return self[name] if key? name
+    self.each { |k,v| return v if k.to_s.to_sym == name }
+    super.method_missing name
+  end
+end
+
 set :sass, { :style => :compact }
 set :haml, { :ugly => true }
 
@@ -30,6 +40,18 @@ helpers do
     end
     flag
   end
+  
+  #Helper to turn date returned from blogger into a ruby date. Assumes Eastern Standard Time (New York)
+  #Assumes format like this: 2009-11-16T23:28:00.000-05:00
+  #
+  #It would be nice to generalize this to deal with any iso formatted string,
+  #make it extend the Time class,
+  #and turn it into a gem.
+  def time_from_iso_format_str(iso_8601_string)
+    iso_8601_string [/(\d+)-(\d+)-(\d+)T(\d+):(\d+):(\d+)/];
+    y=$1;mon=$2;d=$3;h=$4;min=$5;s=$6
+    Time.local(y,mon,d,h,min,s)
+  end
 end
 
 get "/" do
@@ -38,9 +60,12 @@ end
 
 #loads in blogger feed
 get "/thoughts" do
+  #for testing in irb:
+  #require 'open-uri'; require 'crack'; url = 'http://www.blogger.com/feeds/9096209599953091034/posts/default';xml = open(url).read;feed = Crack::XML.parse(xml).feed;posts = feed.entry;
   url = 'http://www.blogger.com/feeds/9096209599953091034/posts/default'
   xml = open(url).read
-  @feed = Crack::XML.parse(xml)
+  feed = Crack::XML.parse(xml).feed
+  @posts = feed.entry
 
   haml :thoughts
 end
