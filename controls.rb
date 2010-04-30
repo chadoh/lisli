ff949102cd1d195439fc09f2f678777575f027f6
Chad Ostrowski <chad.ostrowski@gmail.com>
2010-04-30 00:07:00 -0400

    Locked the bundle...

--- controls.rb
+++ controls.rb
@@ -1,6 +1,12 @@
-require "rubygems"
-require "bundler"
-Bundler.setup
+begin
+  # Require the preresolved locked set of gems.
+  require ::File.expand_path('../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fallback on doing the resolve at runtime.
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+end
 
 require 'sinatra'
 require 'sinatra/base'
