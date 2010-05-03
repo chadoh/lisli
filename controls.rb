f06bc653039cd9061e8b16e4b9d30633d0293ae0
Chad Ostrowski <chad.ostrowski@gmail.com>
2010-05-03 00:59:51 -0400

    ack. i should really configure for development and production. but i should know how production works first, i guess. will mail work NOW???

--- controls.rb
+++ controls.rb
@@ -101,12 +101,7 @@ not_found do
 end
 
 post "/contact" do
-  Pony.mail(:from => 'chad.ostrowski@gmail.com',
-    :to => 'chad.ostrowski@gmail.com',
-    :subject => 'Mail from Lisli.net!',
-    :body => 'Woooo!',
-    :via => :smtp,
-    :via_options => {
+  Pony.mail(:to => 'chad.ostrowski@gmail.com', :from => 'chad@lisli.net', :subject => 'Mail from Lisli.net!', :body => 'Woooo!', :via => :smtp, :via_options => {
       :address => 'smtp.sendgrid.net',
       :port => '25',
       :authentication => :plain,
